67b2e198d6fdbce2027c6fe6c9e7d3d1fd88e06c
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2024-04-25 16:37:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from flask import send_from_directory
+
+UPLOAD_FOLDER = "./Voice"
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/<filename>')
+def uploaded_file(filename):
+  try:
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+  except:
+    return("File Not dound")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, use_reloader=True, threaded=True, debug=True) 
