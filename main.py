7469981f2b4da476e6480368d4ba712fc8dd36fd
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2024-04-25 16:52:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,5 @@ def uploaded_file(filename):
   except:
     return("File Not dound")
 
-def __list_all_voices():
-    mod_paths = glob.glob("./Voice/*.ogg")
-
-    all_modules = [
-        basename(f)[:-3]
-        for f in mod_paths
-        if isfile(f) and f.endswith(".ogg")
-    ]
-
-    return all_voices
-  
-
-@app.route('/ogg')
-def list_files():
-  try:
-    all_voices = sorted(__list_all_voices())
-    return(all_voices)
-  except:
-    return("Error")
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, use_reloader=True, threaded=True, debug=True) 
